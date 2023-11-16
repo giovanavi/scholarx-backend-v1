542222d4d22a7afa9844373233f5813da6d61b90
giovanavi <72512042+giovanavi@users.noreply.github.com>
2023-11-15 21:38:46 -0300

    eliminating code smells

--- src/main/java/org/sefglobal/scholarx/model/Mentee.java
+++ src/main/java/org/sefglobal/scholarx/model/Mentee.java
@@ -11,12 +11,11 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "mentee")
+@Data
+@NoArgsConstructor
 @JsonView(Views.Public.class)
 @JsonIgnoreProperties({"createdAt", "updatedAt"})
 public class Mentee extends EnrolledUser {
-
-    public Mentee() {
-    }
 
     @Column
     private String university;
@@ -48,84 +47,4 @@ public class Mentee extends EnrolledUser {
     @ManyToOne
     private Mentor rejectedBy;
 
-    public Mentor getAppliedMentor() {
-        return appliedMentor;
-    }
-
-    public void setAppliedMentor(Mentor appliedMentor) {
-        this.appliedMentor = appliedMentor;
-    }
-
-    public Mentor getAssignedMentor() {
-        return assignedMentor;
-    }
-
-    public void setAssignedMentor(Mentor assignedMentor) {
-        this.assignedMentor = assignedMentor;
-    }
-
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getIntent() {
-        return intent;
-    }
-
-    public void setIntent(String intent) {
-        this.intent = intent;
-    }
-
-    public String getReasonForChoice() {
-        return reasonForChoice;
-    }
-
-    public void setReasonForChoice(String reasonForChoice) {
-        this.reasonForChoice = reasonForChoice;
-    }
-
-
-    public Mentor getRejectedBy() {
-        return rejectedBy;
-    }
-
-    public void setRejectedBy(Mentor rejectedBy) {
-        this.rejectedBy = rejectedBy;
-    }
-
-    public String getResumeUrl() {
-        return resumeUrl;
-    }
-
-    public void setResumeUrl(String resumeUrl) {
-        this.resumeUrl = resumeUrl;
-    }
-
-    public String getAchievements() {
-        return achievements;
-    }
-
-    public void setAchievements(String achievements) {
-        this.achievements = achievements;
-    }
 }
