5e45d61eebae198c7679ad82736d7b8d295017d7
giovanavi <72512042+giovanavi@users.noreply.github.com>
2023-11-15 21:37:42 -0300

    eliminating code smells

--- src/main/java/org/sefglobal/scholarx/model/Mail.java
+++ src/main/java/org/sefglobal/scholarx/model/Mail.java
@@ -2,41 +2,11 @@ package org.sefglobal.scholarx.model;
 
 import java.util.Map;
 
+@Data
 public class Mail {
     private String emailAddress;
     private String subject;
     private String message;
     private Map<String, Object> props;
 
-    public String getEmail() {
-        return emailAddress;
-    }
-
-    public void setEmail(String email) {
-        this.emailAddress = email;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Map<String, Object> getProps() {
-        return props;
-    }
-
-    public void setProps(Map<String, Object> props) {
-        this.props = props;
-    }
 }
